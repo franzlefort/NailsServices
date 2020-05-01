9666b5edc62cb0098997472dbeb645581857aaa6
Никита Кузин <franzcrypto@gmail.com>
2020-05-01 21:46:18 +0300

    Bind angular and aspnet project

--- AdministrationService/Startup.cs
+++ AdministrationService/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +27,8 @@ namespace AdministrationService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
+            services.AddSpaStaticFiles(configuration => configuration.RootPath = "ClientApp/dist");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +43,21 @@ namespace AdministrationService
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+ 
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+ 
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
 
             app.UseEndpoints(endpoints =>
             {
